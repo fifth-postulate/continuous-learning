12651996db9291421506e6414550bd754da51e4c
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-10-15 21:23:47 +0200

    Implement an update function for Robot

--- src/Robot.elm
+++ src/Robot.elm
@@ -34,3 +34,81 @@ type Message
 type Direction
     = Clockwise
     | AntiClockwise
+
+
+update : Message -> Robot -> Robot
+update message robot =
+    case message of
+        Step ->
+            step robot
+
+        Turn direction ->
+            case direction of
+                Clockwise ->
+                    clockwise robot
+
+                AntiClockwise ->
+                    antiClockwise robot
+
+
+step : Robot -> Robot
+step robot =
+    { robot | position = towards robot.heading robot.position }
+
+
+towards : Heading -> Point -> Point
+towards heading ({ x, y } as point) =
+    case heading of
+        North ->
+            { point | y = y + 1 }
+
+        West ->
+            { point | x = x - 1 }
+
+        South ->
+            { point | y = y - 1 }
+
+        East ->
+            { point | x = x + 1 }
+
+
+clockwise : Robot -> Robot
+clockwise robot =
+    { robot | heading = left robot.heading }
+
+
+left : Heading -> Heading
+left heading =
+    case heading of
+        North ->
+            West
+
+        West ->
+            South
+
+        South ->
+            East
+
+        East ->
+            North
+
+
+antiClockwise : Robot -> Robot
+antiClockwise robot =
+    { robot | heading = right robot.heading }
+
+
+right : Heading -> Heading
+right heading =
+    case heading of
+        North ->
+            East
+
+        West ->
+            North
+
+        South ->
+            West
+
+        East ->
+            South
